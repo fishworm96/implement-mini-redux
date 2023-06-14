35abb3d3889d3ea708b42da597008824ce4de482
fishworm96 <245525860@qq.com>
2023-06-14 16:01:10 +0800

    feat: 使用 Context 读写 appState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-function App() {
+import { useState, useContext, createContext } from "react"
+
+const appContext = createContext(null)
+
+function App () {
+  const [appState, setAppState] = useState({
+    user: { name: "fishworm", age: 18 }
+  })
+  const contextValue = { appState, setAppState }
 
   return (
-    <>
-    </>
+    <appContext.Provider value={contextValue}>
+      <ComponentOne />
+      <ComponentTwo />
+      <ComponentThree />
+    </appContext.Provider>
   )
+}
+
+const ComponentOne = () => <section>组件一<User /></section>
+const ComponentTwo = () => <section>组件二<UserModifier /></section>
+const ComponentThree = () => <section>组件三</section>
+
+const User = () => {
+  const contextValue = useContext(appContext)
+  return <div>User: {contextValue.appState.user.name}</div>
+}
+
+const UserModifier = () => {
+  const contextValue = useContext(appContext)
+  const { appState, setAppState } = contextValue
+  const onChange = e => {
+    appState.user.name = e.target.value
+    setAppState({ ...contextValue.appState })
+  }
+
+  return <div>
+    <input value={contextValue.appState.user.name}
+      onChange={onChange}
+    />
+  </div>
 }
 
 export default App
